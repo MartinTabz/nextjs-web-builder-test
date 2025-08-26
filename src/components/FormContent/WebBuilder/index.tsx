316a0f45009eb95581690d70b8971e38b2053715
Martin Šíl <mail@martinsil.cz>
2025-08-27 01:02:35 +0200

    chore: add showNavigation prop to WebBuilder

--- src/components/FormContent/WebBuilder/index.tsx
+++ src/components/FormContent/WebBuilder/index.tsx
@@ -7,16 +7,22 @@ type Props = {
 	content: string;
 	onChange: (e: string) => void;
 	disabled?: boolean;
+	showNavigation?: boolean;
 };
 
-export default function WebBuilder({ content, onChange, disabled }: Props) {
+export default function WebBuilder({
+	content,
+	onChange,
+	disabled,
+	showNavigation = true,
+}: Props) {
 	return (
 		<div className="fixed top-0 bottom-0 left-0 right-0 z-[20] bg-background overflow-hidden">
 			<EditorProvider>
 				<WebBuilderNavigation />
 
 				<div className="h-full flex justify-center bg-sidebar">
-					<PageEditor liveMode={false} showNavigation={true} />
+					<PageEditor liveMode={false} showNavigation={showNavigation} />
 				</div>
 
 				<PageEditorSidebar />
