8e94ee289386c4c2edab44a398281bf37a488c57
Martin Šíl <mail@martinsil.cz>
2025-08-27 00:59:38 +0200

    feat: adapt dummy site navigation for different devices

--- src/components/FormContent/WebBuilder/PageEditor/DummyComponents/SiteNav.tsx
+++ src/components/FormContent/WebBuilder/PageEditor/DummyComponents/SiteNav.tsx
@@ -1,23 +1,49 @@
+"use client";
+
 import Logo from "@/components/icons/Logo";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useEditor } from "../../Utilities/provider";
+import clsx from "clsx";
 
 export default function SiteNav() {
+	const { state } = useEditor();
+	const { device } = state.editor;
+
+	// Mobile and Tablet = show hamburger, Desktop = show full navigation
+	const isMobileView = device === "Mobile" || device === "Tablet";
+	const isDesktopView = device === "Desktop";
 	return (
 		<header className="h-20 w-full flex items-center justify-center px-2 pointer-events-none">
 			<nav className="w-full max-w-7xl flex items-center justify-between">
 				<div className="flex items-center gap-5">
 					<Link href={"/"}>
-						<Logo className="h-14 lg:h-16 w-auto" />
+						<Logo
+							className={clsx("w-auto", {
+								"h-12": device === "Mobile",
+								"h-14": device === "Tablet",
+								"h-14 lg:h-16": device === "Desktop",
+							})}
+						/>
 					</Link>
-					<div className="hidden lg:flex items-center gap-1">
+					<div
+						className={clsx("items-center gap-1", {
+							hidden: isMobileView,
+							flex: isDesktopView,
+						})}
+					>
 						<LinkItem href={"/"}>Domů</LinkItem>
 						<LinkItem href={"/sluzby"}>Služby</LinkItem>
 						<LinkItem href={"/#o-mne"}>O mně</LinkItem>
 					</div>
 				</div>
-				<div className="hidden lg:flex items-center gap-5">
+				<div
+					className={clsx("items-center gap-5", {
+						hidden: isMobileView,
+						flex: isDesktopView,
+					})}
+				>
 					<div>
 						<LinkItem href={"/blog"}>Blog</LinkItem>
 						<LinkItem href={"/online-kurzy"}>Online kurzy</LinkItem>
@@ -30,7 +56,12 @@ export default function SiteNav() {
 						</Link>
 					</Button>
 				</div>
-				<div className="flex lg:hidden cursor-pointer flex-col gap-2">
+				<div
+					className={clsx("cursor-pointer flex-col gap-2", {
+						flex: isMobileView,
+						hidden: isDesktopView,
+					})}
+				>
 					<div className="h-[2px] w-8 bg-foreground"></div>
 					<div className="h-[2px] w-8 bg-foreground"></div>
 					<div className="h-[2px] w-8 bg-foreground"></div>
@@ -49,7 +80,7 @@ function LinkItem({
 }) {
 	return (
 		<Link
-			className="p-2 font-bold sm:font-bold sm:px-2.5 sm:tracking-tight sm:py-2 sm:hover:text-primary sm:duration-150"
+			className="font-bold px-2.5 tracking-tight py-2 hover:text-primary duration-150"
 			href={href}
 		>
 			{children}
